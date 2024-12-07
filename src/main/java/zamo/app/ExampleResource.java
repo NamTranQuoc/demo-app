a250acb902844f2629f2bb7398e42a102a1a67c2
QuocNam <namtranquoc322@gmail.com>
2024-12-07 17:24:03 +0700

    init

---
+++ src/main/java/zamo/app/ExampleResource.java
@@ -0,0 +1,19 @@
+package zamo.app;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/hello")
+public class ExampleResource {
+    @ConfigProperty(name = "hello.name")
+    String name;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello " + name + " from Quarkus REST";
+    }
+}
